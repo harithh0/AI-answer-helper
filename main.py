fba5f32b2f9fe5cd0d128843d73eed2a3c470415
harithh0 <169714068+harithh0@users.noreply.github.com>
2024-11-17 23:19:42 -0600

    added image recg

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from goprocam import GoProCamera
 import time
+import easyocr
 
 # Connect to GoPro via Wi-Fi
 gopro = GoProCamera.GoPro()
 
 def take_photo():
-	gopro.take_photo(timer=5)
+	gopro.take_photo(timer=1)
 	gopro.downloadLastMedia(custom_filename="selfie.jpg")
-	gopro.shoot_video(10)
-	gopro.downloadLastMedia(custom_filename="selfie.mp4")
+	# gopro.shoot_video(10)
+	# gopro.downloadLastMedia(custom_filename="selfie.mp4")
 
 
 
 take_photo()
+
+
+reader = easyocr.Reader(['en'])
+
+# Path to your image file
+image_path = './selfie.jpg'
+
+# Use EasyOCR to extract text from the image
+results = reader.readtext(image_path)
+
+# Loop through the results and print the extracted text
+for result in results:
+    print(result[1])  # result[1] contains the detected text
